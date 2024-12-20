7942c8ed8cbcc52824dd8b5b547c353c4f4c048a
AfrithHussain <afrithhussain64@gmail.com>
2024-12-20 22:32:44 +0530

    changes added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './Pages/Navbar'
+import ProfileBanner from './Pages/ProfileBanner'
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+          <Navbar/>
+          
+         
+        
+          <Routes>
+            <Route path='/' element={<ProfileBanner/>}/>
+            
+
+          </Routes>
+        
+          
+    </div>
+  )
+}
+
+export default App;
